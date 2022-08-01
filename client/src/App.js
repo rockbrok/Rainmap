6099f58127ea48c3917d24012e798955a4e5caef
paq000 <glennp167@gmail.com>
2022-08-01 14:26:29 -0300

    Update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3>Hello world!</h3>
+        <Card />
       </header>
     </div>
   );
 }
+
+const Card = () => (
+  <div>
+    <h2>Rain of the day</h2>
+    <button>Play audio</button>
+    <button>Copy audio link</button>
+  </div>
+)
 
 export default App;
