c339f7a2ab499781899ae9cf29a172c5c036703e
paq000 <glennp167@gmail.com>
2022-08-01 17:00:45 -0300

    Updated router basename

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <Router basename="/rainmap">
+    <Router basename="/">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
